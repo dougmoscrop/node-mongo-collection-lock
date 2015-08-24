b517263e5958e037b1d6ab0cb477c0aabf224a64
Doug Moscrop <Doug.Moscrop@d2l.com>
2015-08-24 17:29:24 -0400

    Moving to a separate collection for locks vs. on-document

--- test/spec.js
+++ test/spec.js
@@ -8,95 +8,93 @@ require('should');
 
 describe('spinlock', function() {
 	describe('unit', function() {
-		var iterations = 2;
-		var count, update, action;
+		var collection, update, findAndModify, remove, action;
 
-		beforeEach(function(done) {
-			var result = iterations - 1;
-
+		beforeEach(function() {
 			update = sinon.spy(function(query, operation, options, callback) {
 				callback();
 			});
 
-			count = sinon.spy(function(query, callback) {
-				callback(null, result--);
+			findAndModify = sinon.spy(function(query, sort, operation, options, callback) {
+				callback(null, { value: operation });
 			});
 
-			action = sinon.spy(function(id, next) {
-				next();
+			remove = sinon.spy(function(query, callback) {
+				callback();
 			});
 
-			var collection = {
+			action = sinon.spy(function(lock) {
+				lock.release();
+			});
+
+			collection = {
 				update: update,
-				count: count
+				remove: remove,
+				findAndModify: findAndModify
 			};
-
-			spinlock(collection, {
-				try: action
-			}, done);
 		});
 
-		it('should call action each time', function() {
-			assert(action.callCount === iterations);
-		});
+		describe('successful action', function() {
+			beforeEach(function(done) {
+				spinlock(collection)(action, done);
+			});
 
-		it('should call count each time', function() {
-			assert(count.callCount === iterations);
-		});
+			it('gets called', function() {
+				assert(action.called);
+			});
 
-		it('should call update', function() {
-			assert(update.called);
-		});
+			it('calls update', function() {
+				assert(update.called);
+			});
 
-		it('should pass a lock id to the action', function() {
-			assert(action.firstCall.args[0] !== undefined);
-		});
+			it('calls findAndModify', function() {
+				assert(findAndModify.called);
+			});
 
-		it('should ensure all updates are $isolated', function() {
-			assert(update.alwaysCalledWithMatch({ $isolated: 1 }));
-		});
-	});
+			it('calls remove', function() {
+				assert(remove.called);
+			});
 
-	describe('throws when', function() {
-		it('missing collection', function() {
-			assert.throws(function() {
-				spinlock();
+			it('passes a lock to the action', function() {
+				assert(action.firstCall.args[0] !== undefined);
 			});
 		});
 
-		it('missing action', function() {
-			assert.throws(function() {
-				spinlock({}, {});
+		describe('throws when', function() {
+			it('missing collection', function() {
+				assert.throws(function() {
+					spinlock();
+				});
 			});
-		});
-	});
 
-	describe('propagates error', function() {
-		it('when update fails', function(done) {
-			spinlock({
-				update: function(query, operation, options, callback) {
-					callback(new Error());
-				}
-			}, {
-				try: Function.prototype
-			}, function(err) {
-				assert(err);
-				done();
+			it('missing action', function() {
+				assert.throws(function() {
+					spinlock({})();
+				});
 			});
 		});
 
-		it('when action fails', function(done) {
-			spinlock({
-				update: function(query, operation, options, callback) {
-					callback();
-				}
-			}, {
-				try: function(id, next) {
-					next(new Error());
-				}
-			}, function(err) {
-				assert(err);
-				done();
+		describe('propagates error', function() {
+			it('when update fails', function(done) {
+				spinlock({
+					update: function(query, operation, options, callback) {
+						callback(new Error());
+					}
+				})(function() {
+					throw new Error('Should not be called');
+				}, function(err) {
+					assert(err);
+					done();
+				});
+			});
+
+			it('when action fails', function(done) {
+				spinlock(collection)(function(lock) {
+					lock.release(new Error());
+				}, function(err) {
+					assert(err);
+					done();
+				});
 			});
 		});
 	});
@@ -104,7 +102,9 @@ describe('spinlock', function() {
 	(process.env.CI ? describe : describe.skip)('integration', function() {
 		var url = 'mongodb://127.0.0.1:27017/test',
 			db,
-			collection;
+			collection,
+			otherCollection,
+			acquire;
 
 		before(function(done) {
 			MongoClient.connect(url, function(err, db) {
@@ -115,6 +115,7 @@ describe('spinlock', function() {
 
 				db = db;
 				collection = db.collection('collection_lock');
+				otherCollection = db.collection('test_data');
 				done();
 			});
 		});
@@ -128,94 +129,15 @@ describe('spinlock', function() {
 		});
 
 		beforeEach(function(done) {
-			collection.remove({}, function(err) {
-				if (err) {
-					done(err);
-					return;
-				}
-
-				collection.insert([
-					{ foo: 1 },
-					{ foo: 2 },
-					{ bar: 3 },
-					{ baz: 4 }
-				], done);
-			});
+			acquire = spinlock(collection);
+			collection.remove({}, done);
 		});
 
-		it('works on a subset of documents', function(done) {
-			var firstAttempt = true;
+		describe('successful action', function(done) {
 
-			spinlock(collection, {
-				subset: { foo: { $exists: 1 } },
-				try: function(id, next) {
-					collection.find({}).toArray(function(err, results) {
-						if (err) {
-							done(err);
-							return;
-						}
-
-						results.should.have.lengthOf(4);
-						results.forEach(function(result) {
-							if (result.foo) {
-								result.should.have.property('lock');
-
-								if (firstAttempt) {
-									result.lock.should.have.lengthOf(1);
-								}
-							} else {
-								result.should.not.have.property('lock');
-							}
-
-						});
-
-						firstAttempt = false;
-
-						collection.update({ $isolated: 1, 'lock.0.id': id }, { $set: { 'lock.0.released': true } },  { multi: true }, next);
-					});
-				}
-			}, done);
-		});
-
-		it('supports custom completion release signal', function(done) {
-			spinlock(collection, {
-				try: function(id, next) {
-					collection.update({ $isolated: 1, 'lock.0.id': id }, { $set: { 'lock.0.ping': true, 'lock.0.pong': true } }, { multi: true }, next);
-				},
-				until: { ping: true, pong: true }
-			}, done);
-		});
-
-		it('keeps trying', function(done) {
-			var attempts = 0;
-
-			spinlock(collection, {
-				try: function(id, next) {
-					if (attempts < 10) {
-						attempts++;
-						next();
-					} else {
-						collection.update({ $isolated: 1, 'lock.0.id': id }, { $set: { 'lock.0.released': true } },  { multi: true }, next);
-					}
-				}
-			}, function(err) {
-				if (err) {
-					done(err);
-					return;
-				}
-
-				assert(attempts === 10);
-
-				done();
-			});
-		});
-
-		describe('when released', function() {
 			beforeEach(function(done) {
-				spinlock(collection, {
-					try: function(id, next) {
-						collection.update({ $isolated: 1, 'lock.0.id': id }, { $set: { 'lock.0.released': true } },  { multi: true }, next);
-					}
+				acquire(function(lock) {
+					lock.release();
 				}, done);
 			});
 
@@ -226,13 +148,21 @@ describe('spinlock', function() {
 						return;
 					}
 
-					results.should.have.lengthOf(4);
-					results.forEach(function(result) {
-						result.should.have.property('lock');
-						result.lock.should.have.lengthOf(0);
-					});
-
+					results.should.have.lengthOf(0);
 					done();
+				});
+			});
+
+			it('can follow a timeout', function(done) {
+				this.timeout(5000);
+				this.slow(4000);
+
+				acquire(function(outer) {
+					acquire(function(inner) {
+						inner.release();
+					}, done);
+				}, function() {
+					throw new Error('Should not be called');
 				});
 			});
 		});
@@ -242,25 +172,21 @@ describe('spinlock', function() {
 
 			it('update is uniform', function(done) {
 				async.map(['foo', 'bar', 'baz', 'foobar', 'blah', 'many', 'doge', 'mongo'], function(term, callback) {
-					spinlock(collection, {
-						try: function(id, next) {
-							var query = { $isolated: 1, 'lock.0.id': id };
+					acquire(function(lock) {
+						var query = { $isolated: 1 };
 
-							async.series([
-								function(_next) {
-									setTimeout(function() {
-										collection.update(query, { $set: { first: term, 'lock.0.first': true } }, { multi: true }, _next);
-									}, Math.floor((Math.random() * 100) + 1));
-								},
-								function(_next) {
-									setTimeout(function() {
-										collection.update(query, { $set: { second: term, 'lock.0.second': true } }, { multi: true }, _next);
-									}, Math.floor((Math.random() * 100) + 1));
-								}
-
-							], next);
-						},
-						until: { first: true, second: true }
+						async.series([
+							function(_next) {
+								setTimeout(function() {
+									otherCollection.update(query, { $set: { first: term } }, { upsert: true, multi: true }, _next);
+								}, Math.floor((Math.random() * 50) + 1));
+							},
+							function(_next) {
+								setTimeout(function() {
+									otherCollection.update(query, { $set: { second: term } }, { upsert: true, multi: true }, _next);
+								}, Math.floor((Math.random() * 50) + 1));
+							}
+						], lock.release);
 					}, callback);
 				}, function(err) {
 					if (err) {
@@ -268,7 +194,7 @@ describe('spinlock', function() {
 						return;
 					}
 
-					collection.find({}).toArray(function(err, results) {
+					otherCollection.find({}).toArray(function(err, results) {
 						if (err) {
 							done(err);
 							return;
@@ -283,42 +209,6 @@ describe('spinlock', function() {
 						assert(results.every(function(result) {
 							return result.first === results[0].first;
 						}));
-
-						done();
-					});
-				});
-			});
-
-			it('can timeout a lock', function(done) {
-				this.timeout(3000);
-				this.slow(2000);
-
-				spinlock(collection, {
-					ttl: 1,
-					try: function(firstId, next) {
-						spinlock(collection, {
-							try: function(secondId, _next) {
-								collection.update({ $isolated: 1, 'lock.0.id': secondId }, { $set: { 'lock.0.released': true } }, { multi: true }, _next);
-							}
-						}, next);
-					}
-				}, function(err) {
-					if (err) {
-						done(err);
-						return;
-					}
-
-					collection.find({}).toArray(function(err, results) {
-						if (err) {
-							done(err);
-							return;
-						}
-
-						results.should.have.lengthOf(4);
-						results.forEach(function(result) {
-							result.should.have.property('lock');
-							result.lock.should.have.lengthOf(0);
-						});
 
 						done();
 					});
